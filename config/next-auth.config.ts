4f4051c34d23b5c5226c24275e4b39655162764b
CosLynx AI <drixisgod@gmail.com>
2024-09-07 01:27:23 +0530

    generated file: `config/next-auth.config.ts`

---
+++ config/next-auth.config.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/app/db";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "john.doe@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (user && user.password === credentials?.password) {
+          return { id: user.id, name: user.username, email: user.email };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
